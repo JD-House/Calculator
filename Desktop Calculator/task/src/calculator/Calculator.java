18a4a6d6976d180469333aed54ab9df5146e1047
JD-HOUSE <cbrigante@mail.ru>
2022-02-01 05:14:22 +0400

    V3.0

--- Desktop Calculator/task/src/calculator/Calculator.java
+++ Desktop Calculator/task/src/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import org.w3c.dom.events.Event;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,17 +9,16 @@ import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.*;
-import java.util.List;
 
 public class Calculator extends JFrame {
-    final static int BUTTON_WIDTH = 70;
-    final static int BUTTON_HEIGHT = 40;
+    final static int BUTTON_WIDTH = 75;
+    final static int BUTTON_HEIGHT = 50;
 
     private static final int MAIN_FRAME_WIDTH = 350;
-    private static final int MAIN_FRAME_HEIGHT = 500;
+    private static final int MAIN_FRAME_HEIGHT = 600;
 
-    private static final int DELIMITER_X = MAIN_FRAME_HEIGHT / 50 + BUTTON_WIDTH;
-    private static final int DELIMITER_Y = MAIN_FRAME_HEIGHT / 50 + BUTTON_HEIGHT;
+    private static final int DELIMITER_X = MAIN_FRAME_HEIGHT / 160 + BUTTON_WIDTH;
+    private static final int DELIMITER_Y = MAIN_FRAME_HEIGHT / 160 + BUTTON_HEIGHT;
 
 
     private static final int FIRST_ROW =  175;
@@ -25,12 +26,18 @@ public class Calculator extends JFrame {
     private static final int THIRD_ROW =  SECOND_ROW + DELIMITER_Y;
     private static final int FOURTH_ROW =  THIRD_ROW + DELIMITER_Y;
     private static final int FIFTH_ROW =  FOURTH_ROW + DELIMITER_Y;
+    private static final int SIXTH_ROW =  FIFTH_ROW + DELIMITER_Y;
 
     private static final int FIRST_COL = 10;
     private static final int SECOND_COL = FIRST_COL + DELIMITER_X;
     private static final int THIRD_COL =  SECOND_COL + DELIMITER_X;
     private static final int FOURTH_COL = THIRD_COL + DELIMITER_X;
 
+
+    enum buttonWhite {One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero, Dot, PlusMinus}
+
+    private String baseCondition = null;
+    private boolean isNegated = false;
 
     public Calculator() {
         super("Calculator");
@@ -63,7 +70,6 @@ public class Calculator extends JFrame {
 
                                                     //Operators
         //Delete
-
         JButton deleteButton = addButton("Del", "Delete", FOURTH_COL, FIRST_ROW);
         deleteButton.addActionListener(e -> EquationLabel
                 .setText(EquationLabel
@@ -82,8 +88,21 @@ public class Calculator extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 EquationLabel.setText("");
                 ResultLabel.setText("");
+                isNegated = false;
+                baseCondition = null;
             }
         });
+
+        //CE
+        JButton ceButton = addButton("CE", "CE", SECOND_COL, FIRST_ROW);
+        ceButton.addActionListener(e -> EquationLabel
+                .setText(EquationLabel
+                        .getText()
+                        .substring(0, EquationLabel
+                                .getText()
+                                .length() - (EquationLabel
+                                .getText()
+                                .length() > 0 ? 1 : 0))));
 
 
 
@@ -117,7 +136,7 @@ public class Calculator extends JFrame {
         });
 
         //Addition
-        JButton addButton = addButton("\u002B", "Add", FOURTH_COL, FOURTH_ROW);
+        JButton addButton = addButton("\u002B", "Add", FOURTH_COL, FIFTH_ROW);
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -131,7 +150,7 @@ public class Calculator extends JFrame {
         });
 
         //Subtraction
-        JButton subtractButton = addButton("-", "Subtract", FOURTH_COL, FIFTH_ROW);
+        JButton subtractButton = addButton("-", "Subtract", FOURTH_COL, FOURTH_ROW);
         subtractButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -144,74 +163,187 @@ public class Calculator extends JFrame {
             }
         });
 
+        //Sqrt
+        JButton squareRootButton = addButton("\u221A", "SquareRoot", THIRD_COL, SECOND_ROW);
+        squareRootButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    EquationLabel.setText(EquationLabel.getText() + "\u221A(");
+            }
+        });
+
+        //Parentheses
+        JButton parenthesesButton = addButton("( )", "Parentheses", FIRST_COL, FIRST_ROW);
+        parenthesesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                long leftBracketCounter = EquationLabel.getText().chars().filter(ch -> ch == '(').count();
+                long rightBracketCounter = EquationLabel.getText().chars().filter(ch -> ch == ')').count();
+
+                if (leftBracketCounter <= rightBracketCounter ||
+                        EquationLabel.getText().length() > 0 &&
+                                EquationLabel.getText().charAt(EquationLabel.getText().length() - 1) == '(') {
+                    if (EquationLabel.getText().length() > 0) {
+                        if (Parser.isNumber(EquationLabel.getText().substring(EquationLabel.getText().length() - 1))) {
+                            return;
+                        }
+                    }
+                    EquationLabel.setText(EquationLabel.getText() + "(");
+                } else {
+                    EquationLabel.setText(EquationLabel.getText() + ")");
+                }
+            }
+        });
+
+        //PowerTwo
+        JButton powerTwoButton = addButton("X\u00B2", "PowerTwo", FIRST_COL, SECOND_ROW);
+        powerTwoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(EquationLabel.getText().length() == 0) {
+                    return;
+                } else {
+                    EquationLabel.setText(EquationLabel.getText() + "^(2)");
+                    EquationLabel.setText(styleChecker(EquationLabel.getText()));
+                }
+            }
+        });
+
+        //PowerY
+        JButton powerYButton = addButton("X\u02B8", "PowerY", SECOND_COL, SECOND_ROW);
+        powerYButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(EquationLabel.getText().length() == 0) {
+                    return;
+                } else {
+                    EquationLabel.setText(EquationLabel.getText() + "^(");
+                }
+            }
+        });
+
+        //PlusMinus
+        JButton plusMinusButton = addButton("\u00B1", "PlusMinus", FIRST_COL, SIXTH_ROW);
+        plusMinusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isNegated) {
+                    EquationLabel.setText(baseCondition);
+                    baseCondition = null;
+                    isNegated = false;
+                    return;
+                }
+                baseCondition = EquationLabel.getText();
+                isNegated = true;
+
+                StringBuilder equation = new StringBuilder(EquationLabel.getText());
+                if (equation.length() == 0) {
+                    EquationLabel.setText("(-");
+                } else {
+                    if (Character.isDigit(equation.charAt(equation.length() - 1))) {
+                        equation.insert(equation.length() - 1, "(-");
+                    } else if (equation.charAt(equation.length() - 1) == (')')){
+                        int rightParenthesesCounter = 1;
+                        int leftParenthesesCounter = 0;
+                        int pointer = equation.length() - 2;
+                        while (leftParenthesesCounter < rightParenthesesCounter) {
+                            rightParenthesesCounter += equation.charAt(pointer) == (')') ? 1 : 0;
+                            leftParenthesesCounter += equation.charAt(pointer) == ('(') ? 1 : 0;
+                            --pointer;
+                        }
+                        if (pointer < 0) {
+                            equation.insert(0, "(-");
+                            equation.append(")");
+                        } else {
+                            equation.insert(pointer + 1, "(-");
+                        }
+                    }
+                    EquationLabel.setText(equation.toString());
+                }
+            }
+        });
+
         //Equals
-        JButton equalsButton = addButton("=", "Equals", THIRD_COL, FIFTH_ROW);
+        JButton equalsButton = addButton("=", "Equals", FOURTH_COL, SIXTH_ROW);
         equalsButton.addActionListener (new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent event) {
+                long leftBracketCounter = EquationLabel.getText().chars().filter(ch -> ch == '(').count();
+                long rightBracketCounter = EquationLabel.getText().chars().filter(ch -> ch == ')').count();
+
                 String answer = null;
-                try {
-                    answer = solving(Parser.parseInput(EquationLabel.getText()));
-                    if (answer.equals("\u221e") ||
-                            Parser.isOperator(String.valueOf(EquationLabel.getText()
-                                    .charAt(EquationLabel.getText().length() - 1)))) {
+                if (EquationLabel.getText().length() > 0) {
+                    try {
+                        if(leftBracketCounter != rightBracketCounter) {
+                            EquationLabel.setForeground(Color.red.darker());
+                            return;
+                        }
+                        answer = solving(Parser.parseInput(EquationLabel.getText()));
+
+                        if (answer.equals("\u221e") ||
+                                Parser.isOperator(String.valueOf(EquationLabel.getText()
+                                        .charAt(EquationLabel.getText().length() - 1)))) {
+                            EquationLabel.setForeground(Color.red.darker());
+                            return;
+                        }
+                    } catch (ArithmeticException e) {
                         EquationLabel.setForeground(Color.red.darker());
-                        return;
                     }
-                } catch (ArithmeticException e) {
-                    EquationLabel.setForeground(Color.red.darker());
                 }
                 EquationLabel.setForeground(Color.green.darker());
                 ResultLabel.setText(answer);
+
             }
         });
+
+
 
 
                                                     //Numbers
 
         //One
-        JButton buttonOne = addButton("1", "One", FIRST_COL, FOURTH_ROW);
+        JButton buttonOne = addButton("1", "One", FIRST_COL, FIFTH_ROW);
         buttonOne.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonOne.getText()));
 
         //Two
-        JButton buttonTwo = addButton("2", "Two", SECOND_COL, FOURTH_ROW);
+        JButton buttonTwo = addButton("2", "Two", SECOND_COL, FIFTH_ROW);
         buttonTwo.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonTwo.getText()));
 
         //Three
-        JButton buttonThree = addButton("3", "Three", THIRD_COL, FOURTH_ROW);
+        JButton buttonThree = addButton("3", "Three", THIRD_COL, FIFTH_ROW);
         buttonThree.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonThree.getText()));
 
         //Four
-        JButton buttonFour = addButton("4", "Four", FIRST_COL, THIRD_ROW);
+        JButton buttonFour = addButton("4", "Four", FIRST_COL, FOURTH_ROW);
         buttonFour.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonFour.getText()));
 
         //Five
-        JButton buttonFive = addButton("5", "Five", SECOND_COL, THIRD_ROW);
+        JButton buttonFive = addButton("5", "Five", SECOND_COL, FOURTH_ROW);
         buttonFive.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonFive.getText()));
 
         //Six
-        JButton buttonSix = addButton("6", "Six", THIRD_COL, THIRD_ROW);
+        JButton buttonSix = addButton("6", "Six", THIRD_COL, FOURTH_ROW);
         buttonSix.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonSix.getText()));
 
         //Seven
-        JButton buttonSeven = addButton("7", "Seven", FIRST_COL, SECOND_ROW);
+        JButton buttonSeven = addButton("7", "Seven", FIRST_COL, THIRD_ROW);
         buttonSeven.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonSeven.getText()));
 
         //Eight
-        JButton buttonEight = addButton("8", "Eight", SECOND_COL, SECOND_ROW);
+        JButton buttonEight = addButton("8", "Eight", SECOND_COL, THIRD_ROW);
         buttonEight.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonEight.getText()));
 
         //Nine
-        JButton buttonNine = addButton("9", "Nine", THIRD_COL, SECOND_ROW);
+        JButton buttonNine = addButton("9", "Nine", THIRD_COL, THIRD_ROW);
         buttonNine.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonNine.getText()));
 
         //Zero
-        JButton buttonZero = addButton("0", "Zero", SECOND_COL, FIFTH_ROW);
+        JButton buttonZero = addButton("0", "Zero", SECOND_COL, SIXTH_ROW);
         buttonZero.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + buttonZero.getText()));
 
         //Dot
-        JButton dotButton = addButton(".", "Dot", FIRST_COL, FIFTH_ROW);
+        JButton dotButton = addButton(".", "Dot", THIRD_COL, SIXTH_ROW);
         dotButton.addActionListener(e -> EquationLabel.setText(EquationLabel.getText() + dotButton.getText()));
 
         setVisible(true);
@@ -249,15 +381,22 @@ public class Calculator extends JFrame {
 
     private JButton addButton(String text, String name, int col, int row) {
         JButton button = new JButton(text);
-
-
         final Color buttonColor = Color.WHITE;
+        final Color operatorColor = Color.gray.brighter();
 
         button.setName(name);
         button.setBounds(col, row, BUTTON_WIDTH, BUTTON_HEIGHT);
         button.setFocusPainted(false);
-        button.setBackground(buttonColor);
-        button.setFont(new Font(button.getFont().getName(), Font.BOLD, 12));
+
+        button.setBackground(operatorColor);
+
+        for (buttonWhite buttonName : buttonWhite.values()) {
+            if (buttonName.name().equals(name)) {
+                button.setBackground(buttonColor);
+                break;
+            }
+        }
+        button.setFont(new Font(button.getFont().getName(), Font.BOLD, 15));
 
         add(button);
 
@@ -274,6 +413,9 @@ public class Calculator extends JFrame {
                 expression.poll();
                 if(Parser.isNumber(term)) {
                     result.push(Double.parseDouble(term));
+                } else if(Parser.operatorParser(term) == Parser.operators.SQRT) {
+                    a = result.pop();
+                    result.push(Math.sqrt(a));
                 } else if (result.size() > 1){
                     b = result.pop();
                     a = result.pop();
@@ -282,6 +424,7 @@ public class Calculator extends JFrame {
                         case DIVISION : { result.push(a / b); break; }
                         case SUBTRACTION : { result.push (a - b); break; }
                         case ADDITION : { result.push(a + b); break; }
+                        case POWER : { result.push(Math.pow(a, b)); break; }
                     };
                 }
             }
--- Desktop Calculator/task/src/calculator/Parser.java
+++ Desktop Calculator/task/src/calculator/Parser.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public abstract class Parser {
 
-    enum operators {SUBTRACTION, ADDITION, MULTIPLICATION, DIVISION}
+    enum operators {SUBTRACTION, ADDITION, MULTIPLICATION, DIVISION, SQRT, POWER}
 
 
     public static Queue<String> parseInput(String input) {
@@ -12,7 +12,7 @@ public abstract class Parser {
         ArrayDeque<String> postfixExpression = new ArrayDeque<>();
         List<String> expression = new ArrayList<>();
 
-        input.replace("(-", "(0-");
+        input = input.replace("(-", "(0-");
         if (input.charAt(0) == '-') {
             input = "0" + input;
         }
@@ -20,13 +20,13 @@ public abstract class Parser {
         StringBuilder termNumber = new StringBuilder();
         char termTemp;
 
-        for(int i = 0; i < input.length(); ++i) {
+        for (int i = 0; i < input.length(); ++i) {
             termTemp = input.charAt(i);
             if (Character.isDigit(termTemp) || termTemp == '.') {
                 termNumber.append(termTemp);
             } else {
-                if(termNumber.length() > 0) {
-                    if(termNumber.toString().matches("[.][\\d+]")) {
+                if (termNumber.length() > 0) {
+                    if (termNumber.toString().matches("[.][\\d+]")) {
                         termNumber = new StringBuilder("0" + termNumber.toString());
                     }
                     if (termNumber.toString().matches("[\\d=][.]")) {
@@ -38,11 +38,14 @@ public abstract class Parser {
                 expression.add(Character.toString(termTemp));
             }
         }
-        expression.add(termNumber.toString());
+        if (termNumber.length() > 0) {
+            expression.add(termNumber.toString());
+        }
 
         //Shunting-yard
 
-        for (String term : expression) {
+        for (int i = 0; i < expression.size(); ++i) {
+            String term = expression.get(i);
             if(isNumber(term)) {
                 postfixExpression.add(term);
             }else if (isOperator(term)) {
@@ -51,7 +54,32 @@ public abstract class Parser {
                         && getPrecedence(stackOperations.lastElement()) >= getPrecedence(term)) {
                     postfixExpression.add(stackOperations.pop());
                 }
+                if (i < expression.size() - 2 && expression.get(i + 1).equals(")")) {
+                    postfixExpression.add(expression.remove(i + 2));
+                }
+                if (expression.size() > 4 && expression.get(i -1 ).equals("(")) {
+                    postfixExpression.add(expression.remove(i - 4));
+                    stackOperations.push(expression.remove(i - 3));
+                    postfixExpression.add(expression.remove(i - 2));
+                }
                 stackOperations.push(term);
+            }
+            else if (isFunction(term)) {
+                stackOperations.push(term);
+            }
+            else if (isOpenBracket(term)) {
+                stackOperations.push(term);
+            } else if (isCloseBracket(term)) {
+
+                while (!stackOperations.empty()
+                        && !isOpenBracket(stackOperations.lastElement())) {
+                    postfixExpression.add(stackOperations.pop());
+                }
+                stackOperations.pop();
+                if (!stackOperations.empty()
+                        && isFunction(stackOperations.lastElement())) {
+                    postfixExpression.add(stackOperations.pop());
+                }
             }
         }
         while (!stackOperations.isEmpty()) {
@@ -80,6 +108,14 @@ public abstract class Parser {
                 operation = operators.DIVISION;
                 break;
             }
+            case ("^") : {
+                operation = operators.POWER;
+                break;
+            }
+            case ("\u221A") : {
+                operation = operators.SQRT;
+                break;
+            }
             default: operation = null;
         }
         return operation;
@@ -95,7 +131,22 @@ public abstract class Parser {
     }
 
     public static boolean isOperator(String token) {
-        return token.matches("[\u002B\u00D7\u00F7-]");
+        return token.matches("[\u002B\u00D7\u00F7^-]");
+    }
+
+    private static boolean isFunction(String token) {
+        if (token.matches("[\\u221A^]")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOpenBracket(String token) {
+        return token.equals("(");
+    }
+
+    private static boolean isCloseBracket(String token) {
+        return token.equals(")");
     }
 
     private static byte getPrecedence(String token) {
